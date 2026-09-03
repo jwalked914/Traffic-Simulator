c2918274e01820d9244847ad0d314175cbb1d251
Nick Gamble <nick@cpnkirk.com>
2026-09-03 10:51:33 -0500

    Add linear interpretation to smoothen camera movement

--- scenes/CameraController.cs
+++ scenes/CameraController.cs
@@ -2,9 +2,17 @@ using Godot;
 
 public partial class CameraController : Camera2D
 {
-	[Export] 
-	public float MoveSpeed = 600.0f;
+	[Export] public float MoveSpeed = 1200.0f;
+	[Export] public float SmoothSpeed = 16f;
+	[Export] public float SprintMultiplier = 2f;
 	private bool _dragging = false;
+	
+	private Vector2 _targetPosition;
+
+	public override void _Ready()
+	{
+		_targetPosition = Position;
+	}
 
 	public override void _Process(double delta)
 	{
@@ -15,9 +23,16 @@ public partial class CameraController : Camera2D
 			"move_up",
 			"move_down"
 		);
-
+		
+		// check fast camera key and apply multiplier
+		float speedMultiplier = Input.IsActionPressed("fast_camera") ? SprintMultiplier : 1f;
+		
 		// move the camera based on input type and delta time
-		Position += movement * MoveSpeed * (float)delta;
+		_targetPosition += movement * MoveSpeed * speedMultiplier * (float)delta;
+		
+		// Smoothen movement with linear interpretation
+		float t = 1f - Mathf.Exp(-SmoothSpeed * (float)delta);
+		Position = Position.Lerp(_targetPosition, t);
 	}
 
 	public override void _UnhandledInput(InputEvent @event)
